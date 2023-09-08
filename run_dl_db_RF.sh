e1a09edea4e4421b7778c7534187d739669e3c7f
fsmalik <59219252+fsmalik@users.noreply.github.com>
2023-09-08 16:27:26 -0400

    Add files via upload

---
+++ run_dl_db_RF.sh
@@ -0,0 +1,56 @@
+#!/bin/zsh
+
+################################################################################
+# Help                                                                         #
+################################################################################
+Help()
+{
+   # Display Help
+   echo "Runs dl_binder_design for RF diffusion results step by step. All files will end up in the directory with RFdiffusion results"
+   echo
+   echo "Syntax: dl_binder_design_RF.sh /path/to/dir/with/rfdiffusion/results/ /path/to/dl_binder_design/base-dir/ [-h]"
+   echo "options:"
+   echo "  -h     Print this Help."
+   echo
+}
+
+################################################################################
+################################################################################
+# Main program                                                                 #
+################################################################################
+################################################################################
+# Get the options
+while getopts ":h" option; do
+   case $option in
+      h) # display Help
+         Help
+         exit;;
+   esac
+done
+
+RFRESULTSDIR=$1
+DLDESIGNDIR=$2
+
+mkdir $RFRESULTSDIR/pdbs $RFRESULTSDIR/trbs
+mv $RFRESULTSDIR/*.pdb $RFRESULTSDIR/pdbs
+mv $RFRESULTSDIR/*.trb $RFRESULTSDIR/trbs
+
+python $DLDESIGNDIR/helper_scripts/addFIXEDlabels.py --pdbdir $RFRESULTSDIR/pdbs --trbdir $RFRESULTSDIR/trbs
+
+cd $DLDESIGNDIR/include/silent_tools/
+
+./silentfrompdbs $RFRESULTSDIR/pdbs/*.pdb > $RFRESULTSDIR/my_design.silent
+
+cd $RFRESULTSDIR
+
+python $DLDESIGNDIR/mpnn_fr/dl_interface_design.py -silent my_design.silent
+
+python $DLDESIGNDIR/af2_initial_guess/predict.py -silent out.silent
+
+cd $DLDESIGNDIR/include/silent_tools/
+
+./silentextract out.silent
+
+mv *.pdb $RFRESULTSDIR/
+
+cd $RFRESULTSDIR
